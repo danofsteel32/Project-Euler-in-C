e9a585465c912a56ebf497b5b6017a11a4fcff34
danofsteel32 <daniel.batman.davis@gmail.com>
2019-02-11 08:25:33 -0500

    1st

---
+++ 1/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Find the sum of all the multiples of 3 or 5 below 1000 */
+
+int main()
+{
+    int a = 3, b = 5, i, sum = 0;
+
+    for (i = 0; i < 1000; ++i)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum = sum + i;
+        else
+            ;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
